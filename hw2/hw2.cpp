cecc21c2b8f64c3fe1a735303a2a4fa6acc1eb50
Jiodn <mazur1504901@gmail.com>
2023-06-27 14:47:24 -0400

    Сhange the code for the task 1. 2. 3.

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -17,18 +17,22 @@ int main()
 {
     std::cout << "HW 1.\n";
 
-    int a, b;
+    int a, b, temp;
     std::cout << "Enter integer 1: ";
     std::cin >> a;
     std::cout << "Enter integer 2: ";
     std::cin >> b;
+    temp = a;
+    a = b;
+    b = temp;
     std::cout << "Integer 1: " << a << std::endl;
     std::cout << "Integer 2: " << b << std::endl;
 
 
     std::cout << "HW 2.\n";
 
-    double c, ci;
+    double c;
+    int ci;
     std::cout << "Enter number for task 2 ";
     std::cin >> c;
     ci = int(c);
@@ -44,11 +48,12 @@ int main()
     std::cout << "Standart = " << d << std::endl;
     std::cout << "e2 = " << std::scientific << std::setprecision (2) << d << std::endl;
     std::cout << "e5 = " << std::scientific << std::setprecision (5) << d << std::endl;
-    std::cout << "e10 = " << std::scientific << std::setprecision (10) << d << std::endl;
+    std::cout << "e10 = " << std::scientific << d << std::endl;
     
 
     std::cout << "HW 4.\n";
 
+       
     WeekDays day1 = WeekDays::Mon;
     std::cout << int(day1) << "\n";
 
@@ -69,7 +74,7 @@ int main()
 
     WeekDays day7 = WeekDays::Sun;
     std::cout << int(day7) << "\n";
-
+   
 }
  
 
