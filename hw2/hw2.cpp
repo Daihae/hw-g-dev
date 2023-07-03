4ff4fb32821236091ad55d19877d042433cdf5be
Jiodn <mazur1504901@gmail.com>
2023-07-03 16:46:35 -0400

    solved the task4

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -46,35 +46,19 @@ int main()
     double d = 3.14159265358;
 
     std::cout << "Standart = " << d << std::endl;
-    std::cout << "e2 = " << std::scientific << std::setprecision (2) << d << std::endl;
-    std::cout << "e5 = " << std::scientific << std::setprecision (5) << d << std::endl;
+    std::cout << "e2 = " << std::scientific << std::setprecision(2) << d << std::endl;
+    std::cout << "e5 = " << std::scientific << std::setprecision(5) << d << std::endl;
     std::cout << "e10 = " << std::scientific << d << std::endl;
-    
+
 
     std::cout << "HW 4.\n";
 
-       
-    WeekDays day1 = WeekDays::Mon;
-    std::cout << int(day1) << "\n";
+    int w;
+    bool wd = true;
+    w = bool(wd);
+    std::cout << "Enter number for task 4 ";
+    std::cin >> w;
+    std::cout << std::boolalpha << bool(w) << std::endl;
 
-    WeekDays day2 = WeekDays::Tue;
-    std::cout << int(day2) << "\n";
-
-    WeekDays day3 = WeekDays::Wed;
-    std::cout << int(day3) << "\n";
-
-    WeekDays day4 = WeekDays::Thu;
-    std::cout << int(day4) << "\n";
-
-    WeekDays day5 = WeekDays::Fri;
-    std::cout << int(day5) << "\n";
-
-    WeekDays day6 = WeekDays::Sat;
-    std::cout << int(day6) << "\n";
-
-    WeekDays day7 = WeekDays::Sun;
-    std::cout << int(day7) << "\n";
-   
 }
- 
 
